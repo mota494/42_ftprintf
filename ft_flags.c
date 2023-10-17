47584310c98ecf86357701858df65d7d73ed9e2e
Mota494 <migueloureiromota@gmail.com>
2023-10-17 12:01:34 +0100

    Added flags

--- ft_flags.c
+++ ft_flags.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_flags.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mloureir <mloureir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/17 10:28:31 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/17 11:43:40 by mloureir         ###   ########.fr       */
+/*   Created: 2023/10/17 11:44:09 by mloureir          #+#    #+#             */
+/*   Updated: 2023/10/17 11:56:00 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printfh.h"
-#include <stdio.h>
-
-int ft_printf(const char *str, ...)
+void    flags(const char *str, va_list strings)
 {
-    va_list strings;
-    va_start(strings, str);
+    int i;
 
-    return (0);
-}
-
-int main()
-{
-    ft_printf("paulo junior bernardo");
-    return (0);
+    i = 0;
+    while (str[i++] != '\0')
+    {
+        if (str[i] == '%')
+        {
+            if (str[i + 1] == 'd')
+        }
+        else
+            write (1, &str[i], 1);
+    }
 }
--- printfh.h
+++ printfh.h
@@ -6,14 +6,17 @@
 /*   By: mloureir <mloureir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 10:34:09 by mloureir          #+#    #+#             */
-/*   Updated: 2023/10/17 10:54:41 by mloureir         ###   ########.fr       */
+/*   Updated: 2023/10/17 12:01:16 by mloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SGOINFRE_PRINTFH_H
 # define SGOINFRE_PRINTFH_H
 # include <stdarg.h>
+# include <unistd.h>
 
-int ft_printf(const char *str, ...);
+int     ft_printf(const char *str, ...);
+void    flags(const char *str, va_list strings);
+int     ft_printint()
 
 #endif
